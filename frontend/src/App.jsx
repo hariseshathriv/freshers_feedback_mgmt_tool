16c578c56a5cfce60af335c07a2346f72ff0d28e
Pahal Jain <pahajain225@gmail.com>
2024-01-30 15:08:32 +0530

    adding tailwind.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1>Hello, this is a project setup screen!</h1>
     </>
   )
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <h1 className="text-orange-800">JMAN</h1>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
