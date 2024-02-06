2cee8b9989fd25f0ab26f0b5db25594e7a3f5a54
Pahal Jain <pahajain225@gmail.com>
2024-02-06 19:00:43 +0530

    Created  Mentor Registration Page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <nav className='navbar'>
             <div className='navBox'>
                 <a href="/">
-                    <h1 className="text-3xl">JMAN</h1>
+                    <h1 className="text-3xl">JMAN GROUP</h1>
                 </a>
 
                 <span className="text-3xl">FRESHERS FEEDBACK MANAGEMENT TOOL</span>
