ae5ab9d6a1269d221a00dc6edd46623ed36817d0
styx <johithx@gmail.com>
2024-02-06 16:26:34 +0530

    Setup Routes for pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
-import Navbar from './components/Navbar'
-import Login from './components/Login'
-
+// import Navbar from './components/Navbar'
+// import Login from './components/Login'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 function App() {
-
   return (
-    <div className='app'>
-      <Navbar />
-      <Login />
-    </div> 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Login Component</h1>}/>
+        <Route path="/login" element={<h1>Login Component</h1>}/>
+        <Route path="/registration" element={<h1>Registration Component</h1>}/>
+        <Route path="/mentorDashboard" element={<h1>Mentor Component</h1>}/>
+        <Route path="/adminDashboard" element={<h1>Admin Component</h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App
