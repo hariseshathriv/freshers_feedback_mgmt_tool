026796db51f78e59fac6b11b9973d61d4f7e84cd
Pahal Jain <pahajain225@gmail.com>
2024-02-16 21:03:03 +0530

    Added LeftNavbar and designs

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,30 @@
-import React from "react";
-import UserInfo from "./Mentor/UserInfo";
+import React, { useContext } from 'react'
+
+import UserContext from '../context/UserContext';
+import { useLocation } from 'react-router-dom';
 
 function Navbar() {
-  return (
-    <>
-      <nav className="navbar">
-        <div className="navBox">
-          <a href="/">
-            <h1 className="text-3xl">JMAN GROUP</h1>
-          </a>
+  const {user} = useContext(UserContext);
+  const location = useLocation();
+  const currentUrl = location.pathname;
 
-          <span className=" text-3xl">FRESHERS FEEDBACK MANAGEMENT TOOL</span>
+  if (!user) return <div></div>
+
+  return (
+    <nav className='topNav'>
+        <h1 className='text-2xl'>{currentUrl.slice(1)}</h1>
+        <div className="container flex justify-end">
+            <p className="text-2xl mr-10 hover:text-3xl ease-linear transition-all duration-150">
+                 {user && user.data.mentor_name}
+            </p>
+          <button
+              className="text-purple-800 bg-transparent border border-solid border-black hover:bg-purple-800 hover:text-white active:bg-black font-bold uppercase text-xm px-3 py-1 rounded outline-none focus:outline-none mr-3 mb-1 ease-linear transition-all duration-150"
+              type="button"
+          >LOGOUT
+          </button>
         </div>
-      </nav>
-      <UserInfo />
-    </>
-  );
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar
