f8ea3a9345a37383a4e7ead0eec41a75fdaed261
Sudheer Kumar <87257459+sk417@users.noreply.github.com>
2024-02-17 16:33:32 +0530

    API integrated of feedback

--- frontend/src/components/LeftNavbar.jsx
+++ frontend/src/components/LeftNavbar.jsx
@@ -2,11 +2,35 @@ import React, { useContext } from 'react'
 import logo from '../images/logo2.jpeg'
 import {Link, NavLink} from 'react-router-dom'
 import UserContext from '../context/UserContext'
+import { useNavigate } from "react-router-dom";
+
 
 function LeftNavbar() {
     const {user} = useContext(UserContext);
-
+    const navigate = useNavigate();
     if(!user) return <div></div>
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const apiUrl = "http://localhost:3001/api/users/get-mentees/"+user.data.id;
+          const response = await fetch(apiUrl, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+    
+          const data = await response.json();
+
+          user.data.mentees = data.mentees;
+
+          navigate("/feedback");
+
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
 
   return (
     <>
@@ -34,7 +58,7 @@ function LeftNavbar() {
                             `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-purple-800" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-purple-800 lg:p-0`
                             }
                         >
-                            Feedback
+                          <button onClick={handleSubmit}>Feedback</button>
                         </NavLink>
                     </li>
                     <li className='nav-item'>
